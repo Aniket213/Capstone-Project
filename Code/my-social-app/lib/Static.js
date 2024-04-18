be1886a730b6bead2b88423d84abb1f9ddcba700
Aniket Mittal <20BCS6819@cuchd.in>
2024-04-18 12:34:25 +0530

    Add files via upload

---
+++ Code/my-social-app/lib/Static.js
@@ -0,0 +1,47 @@
+export const news = [
+    {
+      category: 'Aniket Mittal',
+      title: 'Ready to Explore new Domains!',
+      image:
+        'https://kajabi-storefronts-production.kajabi-cdn.com/kajabi-storefronts-production/themes/284832/settings_images/rLlCifhXRJiT0RoN2FjK_Logo_roundbackground_black.png',
+    },
+    {
+      category: 'Databases • Trending',
+      title:
+        'Sanity is giving you free Boosted plan, use --coupon cleverprogrammer',
+      image: 'https://avatars.githubusercontent.com/u/17177659?s=280&v=4',
+    },
+    {
+      category: 'Web3.0 • Trending',
+      title: 'Blockchain developers make over $150k',
+      image:
+        'https://cloudfront-us-east-1.images.arcpublishing.com/coindesk/L424HUS65FGRVFPTYYPG24CIYI.jpg',
+    },
+    {
+      category: 'ReactJS • Trending',
+      title: 'Join Profit with React while spots last',
+      image:
+        'https://kajabi-storefronts-production.kajabi-cdn.com/kajabi-storefronts-production/site/18907/products/cIkAZXNyRZimRw4uqXVX_gDvYTtwQsC33KZqCtbpB_image.jpg',
+    },
+  ]
+  
+  export const whoToFollow = [
+    {
+      name: 'Aniket Mittal',
+      handle: '@aniket',
+      avatar:
+        'https://static.vecteezy.com/system/resources/thumbnails/039/845/041/small_2x/flat-illustration-in-grayscale-avatar-user-profile-person-icon-anonymous-profile-profile-picture-for-social-media-profiles-icons-screensaver-free-vector.jpg',
+    },
+    {
+      name: 'David Rakosi',
+      handle: '@0xDavidRakosi',
+      avatar:
+        'https://pbs.twimg.com/profile_images/1282571486465855491/98xO6N0F_400x400.jpg',
+    },
+    {
+      name: 'Sanity.io',
+      handle: '@sanity_io',
+      avatar: 'https://avatars.githubusercontent.com/u/17177659?s=280&v=4',
+    },
+  ]
+  
